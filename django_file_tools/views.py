015de441f282d75e102174c9d9facb8d1d332c2b
Enrico Barzetti <7615875+enricobarzetti@users.noreply.github.com>
2025-03-19 13:19:20 -0700

    Views for temp paths

--- django_file_tools/views.py
+++ django_file_tools/views.py
@@ -1,9 +1,11 @@
 import datetime
+import uuid
 
 from django.conf import settings
 from django.http.response import Http404
 from django.http.response import JsonResponse
 
+from django_file_tools.conf import app_settings
 from django_file_tools.model_fields import TEMP_MARKER
 from django_file_tools.s3 import EXPIRE_FAST
 from django_file_tools.s3 import RETENTION
@@ -61,6 +63,21 @@ def get_s3_signature(request):
         raise Http404
 
     presigned = create_presigned_post(settings.AWS_STORAGE_BUCKET_NAME, name, {RETENTION: EXPIRE_FAST})
+
+    return JsonResponse({
+        'signature': presigned['fields'],
+        'postEndpoint': presigned['url'],
+    })
+
+
+def get_s3_signature_temp(request):
+    """Works with vue-dropzone"""
+    name = request.GET.get('name')
+    if name is None:
+        raise Http404
+
+    path = f'{app_settings.FILE_TOOLS_TEMP_FOLDER_PREFIX}-{uuid.uuid4()}/{name}'
+    presigned = create_presigned_post(settings.AWS_STORAGE_BUCKET_NAME, path, {RETENTION: EXPIRE_FAST})
 
     return JsonResponse({
         'signature': presigned['fields'],
